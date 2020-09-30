d2fbe830bc0b4c717450923293ce2f3a6ee63003
ablue968 <ablue968@gmail.com>
2020-09-30 19:39:43 +0000

    Ejercicio Completado!

---
+++ app.js
@@ -0,0 +1,13 @@
+function mixer(){
+    for (i=1; i < Array.length + 1; i++){
+        let pronoun = ['the','our'];
+        let adj = ['great', 'big' ];
+        let noun = ['jogger','racoon'];
+
+        console.log(sentence);
+    }
+};
+
+var sentence = pronoun+adj+noun;
+
+window.onload 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-require('./style.css')
+require('./style.css');
+
+window.onload = function(){
+    let pronoun = ['the','our'];
+    let adj = ['great', 'big' ];
+    let noun = ['jogger','racoon'];
+    let domain =['.com','.net','.gob']
+    for (i=0; i < pronoun.length; i++){
+        for(i2=0; i2 < adj.length; i2++){
+            for(i3=0; i3 < noun.length; i3++){
+                for(i4=0; i4 < domain.length; i4++){
+                    console.log(pronoun[i]+adj[i2]+noun[i3]+domain[i4]);
+                }
+            }
+        }
+    }
+};
